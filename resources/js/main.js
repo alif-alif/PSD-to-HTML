0554774b5eec0a3144e06524927cacc553c453b3
alif <alif017@gmail.com>
2020-07-12 22:06:42 +0600

    1st project psd to html

---
+++ resources/js/main.js
@@ -0,0 +1,50 @@
+$(document).ready (function() {
+    //sticky menu
+
+    $(".js--services-section").waypoint(function(direction){
+        if(direction == "down"){
+            $("nav").addClass ("sticky");
+        } else {
+            $("nav").removeClass("sticky");
+        }
+
+    });
+
+    //Active link
+    $("nav ul li a").click(function (){
+        $("nav ul li a").removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $("nav a img.logo").click(function (){
+        $("nav ul li a").removeClass("active");
+        $("nav ul li:first-child a").addClass("active");
+    });
+
+    //MIXITUP(portfilo section)
+    var mixes = mixitup('.p2');
+
+    // smooth scroll
+    $("a").on('click', function(event){
+        if(this.hash !== "") {
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html,body').animate({
+                scrollTop: $(hash).offset().top 
+
+                }, 800, function(){
+                    window.location.hash = hash;
+            });
+        }
+    });
+});
+
+function openNav(){
+    document.getElementById("myNav").style.width="100%";
+}
+function closeNav(){
+    document.getElementById("myNav").style.width="0%";
+}
+
